74bd90c89a3b97909d968d3b82490abac2837fc4
Tom Miller <62253468+amillerr@users.noreply.github.com>
2023-08-17 01:40:57 +0300

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Artem Axenov on 2023-08-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
