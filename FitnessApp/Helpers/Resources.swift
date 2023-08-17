e1705bb25e9f003d87604ee186d21ab86b32ae92
Tom Miller <62253468+amillerr@users.noreply.github.com>
2023-08-17 10:39:50 +0300

    feat: add tabbar icons

--- FitnessApp/Helpers/Resources.swift
+++ FitnessApp/Helpers/Resources.swift
@@ -13,4 +13,23 @@ enum Resources {
         static var inActive = UIColor(hexString: "#929DA5")
         static var separator = UIColor(hexString: "#E8ECEF")
     }
+    
+    enum Strings {
+        enum TabBar {
+            static var overview = "Overview"
+            static var session = "Session"
+            static var progress = "Progress"
+            static var settings = "Settings"
+        }
+    }
+    
+    enum Images {
+        enum TabBar {
+            static var overview = UIImage(named: "overview_tab")
+            static var session = UIImage(named: "session_tab")
+            static var progress = UIImage(named: "progress_tab")
+            static var settings = UIImage(named: "settings_tab")
+        }
+
+    }
 }
--- FitnessApp/TabBarController.swift
+++ FitnessApp/TabBarController.swift
@@ -16,6 +16,16 @@ enum Tabs: Int {
 
 final class TabBarController: UITabBarController {
     
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private func configure() {
         tabBar.tintColor = Resources.Colors.active
         tabBar.barTintColor = Resources.Colors.inActive
@@ -29,5 +39,24 @@ final class TabBarController: UITabBarController {
         let sessionController = UIViewController()
         let progressController = UIViewController()
         let settingsController = UIViewController()
+        
+        let overviewNavigation = UINavigationController(rootViewController: overviewController)
+        let sessionNavigation = UINavigationController(rootViewController: sessionController)
+        let progressNavigation = UINavigationController(rootViewController: progressController)
+        let settingsNavigation = UINavigationController(rootViewController: settingsController)
+        
+        //MARK: Create tabbar elements
+        
+        overviewNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.overview, image: Resources.Images.TabBar.overview, tag: Tabs.overview.rawValue)
+        sessionNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.session, image: Resources.Images.TabBar.session, tag: Tabs.session.rawValue)
+        progressNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.progress, image: Resources.Images.TabBar.progress, tag: Tabs.progress.rawValue)
+        settingsNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.settings, image: Resources.Images.TabBar.settings, tag: Tabs.settings.rawValue)
+        
+        setViewControllers([
+            overviewNavigation,
+            sessionNavigation,
+            progressNavigation,
+            settingsNavigation
+        ], animated: false)
     }
 }
